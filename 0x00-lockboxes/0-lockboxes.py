45f947ad5e8130cb39f45cd8439ef71c50090554
juansedev <juanllano93@gmail.com>
2021-07-21 22:57:26 -0500

    delete a line

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-    Module of canUnlockAll
+Module of canUnlockAll
 """
 
 def canUnlockAll(boxes):
     """ validate if all the boxes can be opened
     """
-    arr = [0]
+    opened = [0]
     if (len(boxes) == 0):
         return True
-    for i in arr:
+    for i in opened:
         for i_box in boxes[i]:
-            if i_box not in arr:
+            if i_box not in opened:
                 if i_box < len(boxes):
-                    arr.append(i_box)
-    if len(arr) == len(boxes):
+                    opened.append(i_box)
+    if len(opened) == len(boxes):
         return True
     return False
