1c0cc7d64390462add949c7e4d38f5c798a03ef9
juansedev <juanllano93@gmail.com>
2021-08-05 22:45:50 -0500

    Update

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,18 +1,22 @@
 #include "binary_trees.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * binary_tree_node - inserts a node
- * @parent: pointer to the node to insert the right-child in
- * @value: value to store in the new node
- * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: a pointer to the new node or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
+	new = malloc(sizeof(binary_tree_t));
 	if (!new)
+	{
 		return (NULL);
-
+	}
 
 	new->n = value;
 	new->parent = parent;
--- 0x02-heap_insert/README.md
+++ 0x02-heap_insert/README.md
@@ -1,1 +1,1 @@
-0x02. Heap Insert
+### 0x02. Heap Insert
