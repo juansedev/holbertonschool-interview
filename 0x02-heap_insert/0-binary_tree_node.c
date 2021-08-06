a08d275128b8b393ee76fa3ba8d21a7d65cc51ca
juansedev <juanllano93@gmail.com>
2021-08-05 22:40:38 -0500

    add flexbox project

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new)
 		return (NULL);
 
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -24,6 +24,7 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	c_n = *root;
 
+
 	while (c_n)
 	{
 		if (c_n->left == NULL)
