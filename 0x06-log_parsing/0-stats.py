79a90496efe2e4f3bdef9a4b1070b0b09cfcbf67
juansedev <juanllano93@gmail.com>
2021-09-02 21:21:05 -0500

    Update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,45 @@
 #!/usr/bin/python3
-""" Module for Log Parsing Script """
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
 
 
-if __name__ == '__main__':
-    log = {'200': 0, '301': 0, '400': 0, '401': 0,
-           '403': 0, '404': 0, '405': 0, '500': 0}
-    file_size = 0
-    n_lines = 0
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-    def print_log():
-        """ Method that prints metrics """
-        print("File size: {}".format(file_size))
-        for code in sorted(log.keys()):
-            if log[code]:
-                print("{}: {}".format(code, log[code]))
-    try:
-        for line in sys.stdin:
-            try:
-                response = line.split()
-                code = response[-2]
-                size = response[-1]
-                if code in log:
-                    log[code] += 1
-                    file_size += int(size)
-            except Exception:
-                pass
-            n_lines += 1
-            if n_lines == 10:
-                n_lines = 0
-                print_log()
 
-    except KeyboardInterrupt:
-        print_log()
-        raise
-    else:
-        print_log()
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
