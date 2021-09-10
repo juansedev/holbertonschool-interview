ac7fbd27034f98cb4b8ae327a3f0183dd3d5303e
juansedev <juanllano93@gmail.com>
2021-09-09 19:52:01 -0500

    linked list cycle

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,47 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
-
+"""
+reads stdin line by line and computes metrics
+"""
 import sys
 
+if __name__ == "__main__":
 
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+    file_size = 0
+    count = 0
+    sum_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
 
+    def print_codes(sum_codes, size):
+        """
+        print info
+        """
+        print('File size: {}'.format(size))
+        for code, count in sorted(sum_codes.items()):
+            if count > 0:
+                print('{}: {}'.format(code, count))
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    try:
+        for line in sys.stdin:
+            count += 1
+            input_array = line.split()
+            try:
+                file_size += int(input_array[-1])
+                code = input_array[-2]
+                if code in sum_codes:
+                    sum_codes[code] += 1
+            except Exception:
+                pass
+            if count % 10 == 0:
+                print_codes(sum_codes, file_size)
+    except KeyboardInterrupt:
+        print_codes(sum_codes, file_size)
+        raise
+    print_codes(sum_codes, file_size)
