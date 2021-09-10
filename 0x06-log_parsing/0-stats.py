dcf0d283f42161295b5e746d4ff3698eff805b1d
juansedev <juanllano93@gmail.com>
2021-09-09 20:19:17 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,47 +1,55 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
-"""
+""" Class LogParsing """
+import re
 import sys
 
-if __name__ == "__main__":
 
-    file_size = 0
-    count = 0
-    sum_codes = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+def print_log_parsing(CODES, file_size):
+    """
+    function that print parsing logs
+    args:
+        codes: is a dictionary of status code
+        file_size: is the size of status
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(CODES.items()):
+        print("{}: {}".format(key, value))
 
-    def print_codes(sum_codes, size):
-        """
-        print info
-        """
-        print('File size: {}'.format(size))
-        for code, count in sorted(sum_codes.items()):
-            if count > 0:
-                print('{}: {}'.format(code, count))
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            input_array = line.split()
+def run():
+    """"
+    function that search the status code and size number
+    """
+    PATTERN = '([\\d]{3})\\s([\\d]{1,4})$'
+    CODES = {}
+    STOP = 10
+    step = 1
+    size = 0
+
+    while True:
+
+        try:
+            line = input()
+
+            status, file_size = re.search(PATTERN, line).group().split()
+
+            size += int(file_size)
+
             try:
-                file_size += int(input_array[-1])
-                code = input_array[-2]
-                if code in sum_codes:
-                    sum_codes[code] += 1
-            except Exception:
-                pass
-            if count % 10 == 0:
-                print_codes(sum_codes, file_size)
-    except KeyboardInterrupt:
-        print_codes(sum_codes, file_size)
-        raise
-    print_codes(sum_codes, file_size)
+                if CODES[status]:
+                    CODES[status] += 1
+            except KeyError:
+                CODES[status] = 1
+
+            if step == STOP:
+                print_log_parsing(CODES, size)
+                step = 1
+
+            step += 1
+        except (KeyboardInterrupt, EOFError):
+            print_log_parsing(CODES, size)
+            exit()
+
+
+if __name__ == '__main__':
+    run()
